8decf4114a08dd8d6c560a403d48509d96cd2881
chstrchu <chstrchu@gmail.com>
2014-09-22 21:32:07 -0700

    login/add functions work

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   match 'users/add' => 'users#add', via: [:post]
-  match 'users/login' => 'users#login', via [:post]
+  match 'users/login' => 'users#login', via: [:post]
   
    
   root 'welcome#index'
