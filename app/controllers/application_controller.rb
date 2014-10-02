ae363ffba8beb5788b7afdeada19aa4cf7bc6bc1
chstrchu <chstrchu@gmail.com>
2014-10-01 21:53:44 -0700

    removed access origin stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
   
-  after_action :access_control_headers
-  
-  def set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = "*"
-    headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
-  end
+
 end
