b969f5d960abb821ac38df2e50d592e023ba606b
chstrchu <chstrchu@gmail.com>
2014-10-01 21:50:07 -0700

    added some more access origin stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+  
+  after_action :access_control_headers
+  
   def set_access_control_headers
     headers['Access-Control-Allow-Origin'] = "*"
     headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
